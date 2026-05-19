acf486d0fa84a130468df44c210d1e3fa1c5db5f
zafir <zafirchowdhury69@gmail.com>
2026-05-19 10:42:27 +0600

    Add AGG

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -21,3 +21,19 @@ WHERE url = $1;
 SELECT feeds.*, users.name as user_name
 FROM feeds
 JOIN users ON feeds.user_id = users.id;
+
+
+-- name: MarkFeedFetched :one
+UPDATE feeds 
+SET updated_at = NOW() 
+AND last_fetched_at = NOW()
+WHERE id = $1
+RETURNING *;
+
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at ASC
+NULLS FIRST
+LIMIT 1;
